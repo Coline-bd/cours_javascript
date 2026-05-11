e310c8c4404c45a7b80e5975040fce440f45a926
Coline-bd <coline.bertrand2@gmail.com>
2026-05-11 16:31:33 +0200

    ajout ex calculs

---
+++ Exercices/ex calculs/main.js
@@ -0,0 +1,18 @@
+let addition = 10.5+20;
+let multiplication=20*10.2;
+let calcul1=10*(5+5.5);
+
+console.log(addition);
+console.log(multiplication);
+console.log(calcul1);
+
+console.log((15-5)*20/2);
+
+console.log(2**2);
+
+let compteur=0;
+compteur++;
+compteur+=10;
+compteur--;
+compteur*=2
+console.log(compteur)
