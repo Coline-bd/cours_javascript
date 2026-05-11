46d77750f822adcb1d86a3610999fda2adba9176
Coline-bd <coline.bertrand2@gmail.com>
2026-05-11 14:14:42 +0200

    cration dossier javascript

---
+++ Exercices/exo-variables/main.js
@@ -0,0 +1,24 @@
+let string="string";
+console.log(string)
+let nombre=23;
+console.log(nombre)
+let nombreVirgule=23.7;
+console.log(nombreVirgule)
+let table=[1,2,3,4,5];
+console.log(table)
+console.log(table[2])
+
+let unObjet={
+    prenom: "coline",
+    age: 29,
+}
+console.log(unObjet)
+console.log(unObjet.prenom)
+
+
+let affichage = function afficher (){
+    console.log("hello world");
+}
+
+console.log(affichage)
+
