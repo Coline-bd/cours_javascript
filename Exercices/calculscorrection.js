05e2077ce405b104abac7fee7a1bf4debc142f84
Coline-bd <coline.bertrand2@gmail.com>
2026-05-12 11:12:39 +0200

    mise à jour fichiers + ajout strings

---
+++ Exercices/calculscorrection.js
@@ -0,0 +1,41 @@
+console.log("-------------------Exercices Calculs-------------------");
+
+console.log('2 puissance 10 = ',2**10);
+
+console.log('20 + 33 = ',20+33);
+console.log('20 - 33 = ',20-33);
+console.log('20 / 33 = ',20/33);
+console.log('20 * 33 = ',20*33);
+// 🚨 Troll pour les décimales
+console.log('(piège)2,33+10 = ',2,33+10);
+// Pour les décimales la virgule avec la notation en .
+console.log('2.33+10 = ',2.33+10);
+console.log('0.1+0.2 = ',0.1+0.2);
+console.log('1+20*5 = ',1+20*5);
+console.log('(1+20)*5 = ',(1+20)*5);
+console.log('10%2 = ',10%2);
+console.log('3%2 = ',3%2);
+
+// Incrémentation
+let num = 0;
+console.log('num+1 = ',num+1);
+//? Pour faire du Cumul : 
+// num = num+1;
+num +=1;//Notation raccourcie (assignation composé)
+// num = num + 5;
+num +=5;
+// Notation raccourcie division par 2
+num/=2;
+// Notation raccourcie multiplication par 2
+num*=2;
+// Notation raccourcie soustraction par 1
+num-=1;
+// Notation raccourcie addition par 1
+num+=1;
+// Notation raccourcie modulo par 2
+num%=2;
+// Notation raccourcie exponentiation par 2
+num**=2;
+// Notation raccourcie incrémentation de 1
+console.log('num++ = ',num++);
+console.log('num-- = ',num--);
---
+++ Exercices/strings.js
@@ -0,0 +1,16 @@
+let pizarria ='"La pizarria Raffinata"';
+let date = "12/05/2026";
+let prenom = "Giselle";
+let age = 70;
+let adresse = "10 boulevard de la mama";
+let temps = 30;
+
+let blague ="Qu'est ce qu'une pizza dit à une autre pizza qui lui demandait des conseils ? \"Suis ta pate et tout ira bien\" !"
+// let bio = `${prenom}, ${age} ans, 
+// habite à ${ville}`;
+
+let pizza = "margarita";
+
+let SumUpOrderPhrase ="Bonjour " + prenom + ", votre pizza : " + pizza + " est en cours de préparation. Elle vous sera livrée d'ici " + temps+ "minutes au " + adresse + " merci d'avoir commandé chez " + pizarria + ". \n --- \n" + blague
+
+console.log(SumUpOrderPhrase)
