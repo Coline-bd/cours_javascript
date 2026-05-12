2f491451802e17ffdaa6038f7a0728f5278bcdfb
Coline-bd <coline.bertrand2@gmail.com>
2026-05-12 12:06:57 +0200

    feat: ajout array.html et array.js

---
+++ Exercices/array.js
@@ -0,0 +1,9 @@
+let nom = "Giselle";
+let age = 70;
+let passions = ["tricot","motocross"]
+
+let tabUser =[nom,age,passions]
+
+console.log(tabUser)
+console.log(tabUser[2])
+console.log(tabUser[2][1])
