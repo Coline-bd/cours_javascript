3b9f5ee9430cd098d9ae1ebb0308ef5845a79798
Coline-bd <coline.bertrand2@gmail.com>
2026-05-13 12:13:20 +0200

    feat(boucles): ajout conditions

--- Exercices/boucle.js
+++ Exercices/boucle.js
@@ -4,10 +4,33 @@ function calculerMoyenne(tableau=[]){
     let somme=0;
     let n=tableau.length;
     for (let i=0;i<n;i++){
-        somme+=tableau[i]
+        somme+=tableau[i];
     }
-    return somme/n
+    return somme/n;
 }
 
-let moyenne=calculerMoyenne(notes)
-console.log(moyenne)
+// let moyenne=calculerMoyenne(notes);
+// console.log(moyenne);
+
+
+/*Avec des conditions*/
+function calculerMoyenneMention(tableau=[]){
+    let somme=0;
+    let n=tableau.length;
+    for (let i=0;i<n;i++){
+        somme+=tableau[i];
+    }
+    let moyenne=somme/n;
+    if (moyenne>=15){
+        return "Très bien"
+    }
+    else if (moyenne>=10){
+        return "Assez bien"
+    }
+    else{
+        return "Refusé"
+    }
+}
+
+resultat=calculerMoyenneMention(notes)
+console.log(resultat)
